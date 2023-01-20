b309ae69a6835d4b0ca4f3dfaad0d3e42ec1856e
Lucas <lucasandim@yahoo.com.br>
2023-01-20 18:48:41 -0400

    calculadora

---
+++ calculadora/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1, v2, soma;
+
+            v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            v2 = float.Parse(Console.ReadLine());
+
+            soma = v1 + v2;
+
+            Console.WriteLine(soma);
+        }
+    }
+}
